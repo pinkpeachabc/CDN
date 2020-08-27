4ae8935961731114fee7533706e8e6a9c0b99290
pinkpeachabc <44872954+pinkpeachabc@users.noreply.github.com>
2020-08-27 13:05:35 +0800

    音乐2

--- js/12.js
+++ js/12.js
@@ -1,12 +1,9 @@
 <meting-js
-	name="rainymood"
-	artist="rainymood"
-	url="https://rainymood.com/audio1110/0.m4a"
-	cover="https://rainymood.com/i/badge.jpg"
-	fixed="true">
-	<pre hidden>
-		[00:00.00]This
-		[00:04.01]is
-		[00:08.02]lyric
-	</pre>
+	server="netease"
+	type="playlist"
+    id="60198"
+    listfolded="true"
+    order="list"
+	fixed="true"
+    theme="#2196f3">
 </meting-js>
