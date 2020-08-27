03240a40f66b60011b21c0c856daa3501b6e6613
pinkpeachabc <44872954+pinkpeachabc@users.noreply.github.com>
2020-08-27 10:39:03 +0800

    音乐2

--- js/music.js
+++ js/music.js
@@ -1,10 +1,10 @@
-	name="rainymood"
-	artist="rainymood"
-	url="https://rainymood.com/audio1110/0.m4a"
-	cover="https://rainymood.com/i/badge.jpg"
-	fixed="true">
-	<pre hidden>
-		[00:00.00]This
-		[00:04.01]is
-		[00:08.02]lyric
-	</pre>
+name="rainymood"
+artist="rainymood"
+url="https://rainymood.com/audio1110/0.m4a"
+cover="https://rainymood.com/i/badge.jpg"
+fixed="true">
+<pre hidden>
+	[00:00.00]This
+	[00:04.01]is
+	[00:08.02]lyric
+</pre>
